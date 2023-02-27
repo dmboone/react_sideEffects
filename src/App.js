54c29524c6c311e790530c0f59e17be1a8184fc1
Destiny Boone <destaj.db@gmail.com>
2023-02-27 13:05:19 -0500

    using the useEffect() hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,23 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => { // will run this code any time the code reloads or any time one of the dependencies listed in the array is altered
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if(storedUserLoggedInInformation ===  '1'){
+      setIsLoggedIn(true);
+    };
+  }, []); // here is the array of dependencies the code above will depend on
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1'); // 1 means true in this case
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
