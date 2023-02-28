99675f484d7182c726cef5f65e7cacf10fd605a4
Destiny Boone <destaj.db@gmail.com>
2023-02-27 19:28:04 -0500

    using the useReducer() hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+// can create the reducer function outside of the component function 
+// because we don't need any data created inside of the component function
+const emailReducer = (state, action) => { // takes in our last state snapshot and the action that was dispatched
+  if(action.type === 'USER_INPUT'){
+    return {value: action.val, isValid: action.val.includes('@')};
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return {value: state.value, isValid: state.value.includes('@')} // the state var that React provides here is guaranteed to always be the most up to date
+  }
+  return {value: '', isValid: false}; // returns a new state
+}; 
+                              
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: null});
 
   useEffect(()=> { // if no dependencies given, useEffect will run everytime the component function re-runs
     console.log('EFFECT RUNNING - NO DEPENDENCIES GIVEN');
@@ -23,30 +38,42 @@ const Login = (props) => {
     console.log('EFFECT RUNNING - DEPENDENCY FOR PASSWORD GIVEN');
   }, [enteredPassword]);
 
-  useEffect(() => { // use whenever you have an action that should be executed in response to some other action
-    const identifier = setTimeout(()=> { // *debouncing* so we don't check at literally every keystroke but only after some time (5 sec) has passed
-      console.log('Checking form validity!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6 // helps to eliminate repetitive code that we use in both emailChangeHandler and passwordChangeHandler
-      );
-    }, 500);
+  // we are replacing the useEffect solution with a useReducer solution because it's not good practice to make changes 
+  // based on prev state without first copying prev state which is not possible in this example since our changes 
+  // depend on multiple states at the same time. 
+  // For example, the email state may be the most up to date but the password state may not and vice versa, and because
+  // our formIsValid state depends on both our other state-managed variables enteredEmail and enteredPassword we can not use our regular copy over function
+  // like we normally would.
+  // useReducer provides an option for these more complex scenarios
+
+  // useEffect(() => { // use whenever you have an action that should be executed in response to some other action
+  //   const identifier = setTimeout(()=> { // *debouncing* so we don't check at literally every keystroke but only after some time (5 sec) has passed
+  //     console.log('Checking form validity!');
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6 // helps to eliminate repetitive code that we use in both emailChangeHandler and passwordChangeHandler
+  //     );
+  //   }, 500);
     
-    return () => { // just a cleanup function that runs before useEffect executes again
-      console.log('CLEANUP');
-      clearTimeout(identifier); // we are essentially clearing the previous timer before we set a new one
-    };
-  }, [enteredEmail, enteredPassword]);
+  //   return () => { // just a cleanup function that runs before useEffect executes again
+  //     console.log('CLEANUP');
+  //     clearTimeout(identifier); // we are essentially clearing the previous timer before we set a new one
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid && enteredPassword.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
@@ -55,7 +82,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -63,14 +90,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
